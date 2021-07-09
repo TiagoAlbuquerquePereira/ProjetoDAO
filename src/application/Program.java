a29f7bbbfc9a114ec197e3a54ac98dce997bf8b5
Tiago <Tiago@192.168.0.4>
2021-07-09 11:11:57 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,10 @@ public class Program {
 
 		Department dp = new Department(1, "Tiago Albuquerque");
 		System.out.println(dp);
+		
+		Seller seller = new Seller(1, "joão", "joao@gmail.com", new Date(), 3000.0, dp);
+		System.out.println(seller);
 
 	}
-
+ 
 }
